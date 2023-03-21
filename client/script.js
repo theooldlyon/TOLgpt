2bdd5715246bafed11f26d8d619eb9e23a65b1ad
Luke4G1 <theooldlyon@gmail.com>
2023-03-21 15:29:28 +0100

    ciao

--- client/script.js
+++ client/script.js
@@ -1769,6 +1769,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   const response = await fetch('https://tolgpt.onrender.com', {
+
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
